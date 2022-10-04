8c2ba2d9d4ab29b87d9591d8ab9b020dc4e2e330
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-10-04 13:49:41 +0100

    Implement unary add 1

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from dataclasses import dataclass
+from enum import Enum
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class TapeMovement(Enum):
+    LEFT = 0
+    RIGHT = 1
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@dataclass
+class TransitionFunction:
+    from_state: int
+    current_value: str
+    to_state: int | None
+    action: TapeMovement | str
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+transition_functions = [
+    TransitionFunction(0, "", 1, TapeMovement.RIGHT),
+    TransitionFunction(0, "1", 1, TapeMovement.RIGHT),
+    TransitionFunction(1, "", 2, "1"),
+    TransitionFunction(1, "1", 1, TapeMovement.RIGHT),
+    TransitionFunction(2, "", None, ""),
+    TransitionFunction(2, "1", 2, TapeMovement.LEFT),
+]
+
+tfd: dict[int, dict[str, tuple[int | None, TapeMovement | str]]] = {}
+for tf in transition_functions:
+    if tf.from_state not in tfd:
+        tfd[tf.from_state] = {}
+    tfd[tf.from_state][tf.current_value] = (tf.to_state, tf.action)
+
+tape: list[str] = ["", "1", "1", ""]
+tape_index = 0
+state = 0
+
+print(tape)
+
+while state is not None:
+    to_state, action = tfd[state][tape[tape_index]]
+
+    # print(tape)
+    # print(to_state, action)
+    # print()
+
+    if type(action) == str:
+        tape[tape_index] = action
+    elif action == TapeMovement.LEFT:
+        tape_index -= 1
+    elif action == TapeMovement.RIGHT:
+        tape_index += 1
+        if tape_index == len(tape):
+            tape.append("")
+    else:
+        raise Exception("Invalid action")
+
+    state = to_state
+
+print(tape)
