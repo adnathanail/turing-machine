e83b8935dcd03348cdef21529754d85fee88e3b7
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-10-07 13:10:12 +0100

    Refactor turing machine into function

--- main.py
+++ main.py
@@ -15,7 +15,36 @@ class TransitionFunction:
     action: TapeMovement | str
 
 
-transition_functions = [
+def turing_machine(transition_functions: list[TransitionFunction], tape: list[str]) -> list[str]:
+    tfd: dict[int, dict[str, tuple[int | None, TapeMovement | str]]] = {}
+    for tf in transition_functions:
+        if tf.from_state not in tfd:
+            tfd[tf.from_state] = {}
+        tfd[tf.from_state][tf.current_value] = (tf.to_state, tf.action)
+
+    tape_index = 0
+    state = 0
+
+    while state is not None:
+        to_state, action = tfd[state][tape[tape_index]]
+
+        if type(action) == str:
+            tape[tape_index] = action
+        elif action == TapeMovement.LEFT:
+            tape_index -= 1
+        elif action == TapeMovement.RIGHT:
+            tape_index += 1
+            if tape_index == len(tape):
+                tape.append("")
+        else:
+            raise Exception("Invalid action")
+
+        state = to_state
+
+    return tape
+
+
+tfs = [
     TransitionFunction(0, "", 1, TapeMovement.RIGHT),
     TransitionFunction(0, "1", 1, TapeMovement.RIGHT),
     TransitionFunction(1, "", 2, "1"),
@@ -23,37 +52,6 @@ transition_functions = [
     TransitionFunction(2, "", None, ""),
     TransitionFunction(2, "1", 2, TapeMovement.LEFT),
 ]
-
-tfd: dict[int, dict[str, tuple[int | None, TapeMovement | str]]] = {}
-for tf in transition_functions:
-    if tf.from_state not in tfd:
-        tfd[tf.from_state] = {}
-    tfd[tf.from_state][tf.current_value] = (tf.to_state, tf.action)
-
-tape: list[str] = ["", "1", "1", ""]
-tape_index = 0
-state = 0
-
-print(tape)
-
-while state is not None:
-    to_state, action = tfd[state][tape[tape_index]]
-
-    # print(tape)
-    # print(to_state, action)
-    # print()
-
-    if type(action) == str:
-        tape[tape_index] = action
-    elif action == TapeMovement.LEFT:
-        tape_index -= 1
-    elif action == TapeMovement.RIGHT:
-        tape_index += 1
-        if tape_index == len(tape):
-            tape.append("")
-    else:
-        raise Exception("Invalid action")
-
-    state = to_state
-
-print(tape)
+t: list[str] = ["", "1", "1", ""]
+print(t)
+print(turing_machine(tfs, t))
